70f349449e6af917673395a2a3c11581135e8aa5
Over9620 <axelfr1040@gmail.com>
2026-03-31 21:31:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from clara_web import handle_clara_request
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static")
+
+@app.get("/")
+def serve_ui():
+    return send_from_directory("static", "index.html")
 
 @app.post("/clara")
 def clara_endpoint():
